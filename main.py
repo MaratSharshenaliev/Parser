f599545856016d123187dfb1e6a5cadde5c6c719
sharshenaliev.2003gmail.com <sharshenaliev.2003gmail.com>
2022-09-23 02:30:48 +0600

    changed a lot of algorithm

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import requests
 import dateparser
 from typing import List, Dict
 import csv
-
+from model import Data
+import datetime
 
 headers = {
     "Accept": "*/*",
@@ -12,18 +13,20 @@ headers = {
 }
 
 
-def saveTocsv(data: Dict):
+def saveTocsv(data: Dict) -> int:
     with open("data.csv", "a") as file:
         writer = csv.writer(file)
         writer.writerow((data['link_of_photo'], data['date_posted'], data['price']))
 
+    return 0
 
-def getData():
+
+def getData() -> int:
+    global somethingI
     for i in range(1, 94):
         BASE_URL = f"https://www.kijiji.ca/b-apartments-condos/city-of-toronto/page-{i}/c37l1700273"
         res = requests.get(BASE_URL, headers)
         soup = BS(res.content, 'html.parser')
-
         for photo, date_posted, price in zip(soup.select(".image > picture > source"),
                                              soup.select(".location > .date-posted"),
                                              soup.select(".info-container > .price")):
@@ -36,15 +39,45 @@ def getData():
             if date_posted not in "/":
                 dt = dateparser.parse(date_posted.replace("<", ""))
                 date_posted = dt.strftime("%d/%m/%Y")
-                print(date_posted)
 
             data = {
                 "link_of_photo": photo,
                 "date_posted": date_posted,
                 "price": price
             }
-            saveTocsv(data)
+            somethingI = saveTocsv(data)
+        print(f"[INFO] Обработано - {i} страницы")
+    return 0 if somethingI == 0 else 1
+
+
+def save_database(data: str):
+    link_of_photo = data["link_of_photo"]
+    readyDate = [int(i) for i in data["date_posted"].split("/")][::-1]
+    date_posted = datetime.date(readyDate[0], readyDate[1], readyDate[2])
+    price = float(data["price"].replace(",", "")[1:]) if data["price"] != "Please Contact" else 0
+
+    if len(str(price)) > 7:
+        price = str(price)[:-3]
+
+    print(link_of_photo, date_posted, price)
+    # Data(
+    #     link_of_photo=link_of_photo,
+    #     date_posted=date_posted,
+    #     price=price
+    # ).save()
+
+
+def save():
+    with open("data.csv") as f:
+        keys = ["link_of_photo", "date_posted", "price"]
+        reader_baike = csv.DictReader(f, fieldnames=keys)
+        data = list(reader_baike)
+
+        for pack in data:
+            save_database(pack)
 
 
 if __name__ == '__main__':
-    getData()
+    somethingO = getData()
+    if somethingO == 0:
+        save()
---
+++ model.py
@@ -0,0 +1,29 @@
+from peewee import Model, \
+    CharField, \
+    DecimalField, \
+    DateField, \
+    PostgresqlDatabase
+
+db = PostgresqlDatabase(
+    database="HouseDB",
+    password="Redux200316",
+    user="postgres",
+    host="localhost",
+    port=5432
+)
+
+
+class Data(Model):
+    link_of_photo = CharField(max_length=255)
+    date_posted = DateField()
+    price = DecimalField()
+
+    class Meta:
+        database = db
+
+
+db.connect()
+db.create_tables([Data])
+
+
+
